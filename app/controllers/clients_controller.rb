1b31272381c00bcb10cad6ddf7a99bf0c7878517
sentamu Judah <sentsjman1@gmail.com>
2017-04-15 07:57:18 +0300

    client show

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -28,7 +28,7 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
+        format.html { redirect_to @client, notice: 'Message was successfully Sent, We will get back to You as soon as Possible.' }
         format.json { render :show, status: :created, location: @client }
       else
         format.html { render :new }
